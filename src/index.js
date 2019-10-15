4147dc7cae0a810aa46fb445a7987abfc3cad3d4
John Knotts <jhnnyk@gmail.com>
2019-10-15 17:11:05 -0600

    resource reading endpoints: part 2

--- src/index.js
+++ src/index.js
@@ -23,12 +23,8 @@ app.post('/users', (req, res) => {
 
 app.get('/users', (req, res) => {
   User.find({})
-    .then(users => {
-      res.send(users);
-    })
-    .catch(e => {
-      res.status(500).send();
-    });
+    .then(users => res.send(users))
+    .catch(e => res.status(500).send());
 });
 
 app.get('/users/:id', (req, res) => {
@@ -42,9 +38,7 @@ app.get('/users/:id', (req, res) => {
 
       res.send(user);
     })
-    .catch(e => {
-      res.status(500).send();
-    });
+    .catch(e => res.status(500).send());
 });
 
 app.post('/tasks', (req, res) => {
@@ -55,9 +49,27 @@ app.post('/tasks', (req, res) => {
     .then(() => {
       res.status(201).send(task);
     })
-    .catch(e => {
-      res.status(400).send(e);
-    });
+    .catch(e => res.status(400).send(e));
+});
+
+app.get('/tasks', (req, res) => {
+  Task.find({})
+    .then(tasks => res.send(tasks))
+    .catch(e => res.status(500).send());
+});
+
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then(task => {
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch(e => res.status(500).send());
 });
 
 app.listen(port, () => {
