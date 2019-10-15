1b6c47a8f6f0688381f56e197167b551a2f36729
John Knotts <jhnnyk@gmail.com>
2019-10-15 11:53:11 -0600

    resource creation endpoints: part 1

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,48 +1,8 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true
-});
-
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Email is invalid');
-      }
-    }
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error('Password cannot contain "password"');
-      }
-    }
-  },
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number');
-      }
-    }
-  }
 });
 
 const Task = mongoose.model('Task', {
@@ -56,24 +16,3 @@ const Task = mongoose.model('Task', {
     default: false
   }
 });
-
-// const me = new User({
-//   name: '  John  ',
-//   email: 'MyEmail@EMAIL.com   ',
-//   password: ' PssWrd23   '
-// });
-
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch(error => console.log('Error!', error));
-
-const errand = new Task({
-  description: 'buy doorknob  '
-});
-
-errand
-  .save()
-  .then(console.log(errand))
-  .catch(error => console.log(error));
