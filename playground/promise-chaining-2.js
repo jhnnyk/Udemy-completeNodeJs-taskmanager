da99d901ece4205f62b07bce03d73daa5892123a
John Knotts <jhnnyk@gmail.com>
2019-10-17 09:52:23 -0600

    async-await part 2

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,12 +1,22 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndDelete('5da618a8ac8c0b688c1eb034')
-  .then(task => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then(result => {
-    console.log(result);
-  })
+// Task.findByIdAndDelete('5da618a8ac8c0b688c1eb034')
+//   .then(task => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(e => console.log(e));
+
+const deleteTaskAndCount = async id => {
+  const task = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: false });
+  return count;
+};
+
+deleteTaskAndCount('5da616fb4dc95c686e579ec3')
+  .then(count => console.log(count))
   .catch(e => console.log(e));
