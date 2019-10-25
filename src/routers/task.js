4969d127ea10bdb11b5768b0e8d6b39a0068fa4e
John Knotts <jhnnyk@gmail.com>
2019-10-25 17:27:00 -0600

    paginating data

--- src/routers/task.js
+++ src/routers/task.js
@@ -18,6 +18,7 @@ router.post('/tasks', auth, async (req, res) => {
 });
 
 // GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
 router.get('/tasks', auth, async (req, res) => {
   const match = {};
 
@@ -29,7 +30,11 @@ router.get('/tasks', auth, async (req, res) => {
     await req.user
       .populate({
         path: 'tasks',
-        match
+        match,
+        options: {
+          limit: parseInt(req.query.limit),
+          skip: parseInt(req.query.skip)
+        }
       })
       .execPopulate();
     res.send(req.user.tasks);
