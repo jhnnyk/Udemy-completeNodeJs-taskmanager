2c19d54a91ed095333eae63200a9a25822c3a569
John Knotts <jhnnyk@gmail.com>
2019-10-28 13:30:15 -0600

    reverting changes

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  })
-  .catch(e => console.log(e));
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
